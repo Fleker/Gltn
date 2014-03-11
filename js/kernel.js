a69b6ac93ad964bb737b44a0aa5dc6dc86d608c9
Fleker <handnf@gmail.com>
2014-03-11 08:54:30 -0400

    Fullscreen API

--- js/kernel.js
+++ js/kernel.js
@@ -2705,4 +2705,42 @@ function getLoader(query, m) {
          return "<div class='loader10' style='margin-left:"+mL+"px;margin-top:"+mT+"'></div>";
          */
 }
+ function cancelFullScreen(el) {
+            var requestMethod = el.cancelFullScreen||el.webkitCancelFullScreen||el.mozCancelFullScreen||el.exitFullscreen;
+            if (requestMethod) { // cancel full screen.
+                requestMethod.call(el);
+            } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+                var wscript = new ActiveXObject("WScript.Shell");
+                if (wscript !== null) {
+                    wscript.SendKeys("{F11}");
+                }
+            }
+        }
+
+        function requestFullScreen(el) {
+            // Supports most browsers and their versions.
+            var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen;
+
+            if (requestMethod) { // Native full screen.
+                requestMethod.call(el);
+            } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+                var wscript = new ActiveXObject("WScript.Shell");
+                if (wscript !== null) {
+                    wscript.SendKeys("{F11}");
+                }
+            }
+            return false
+        }
+
+        function toggleFull() {
+            var elem = document.body; // Make the body go full screen.
+            var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) ||  (document.mozFullScreen || document.webkitIsFullScreen);
+
+            if (isInFullScreen) {
+                cancelFullScreen(document);
+            } else {
+                requestFullScreen(elem);
+            }
+            return false;
+        }
 
